e93ce251943113fb5d2c190118b907618c803f7b
cdeanj <dean.chris.j@gmail.com>
2018-03-20 12:32:31 -0600

    insertion consumes query -- not reference too

--- resistome.cpp
+++ resistome.cpp
@@ -52,13 +52,10 @@ void resistome::process_gene_level(std::map<std::string, record> &records, const
                                 	break;
                         	case 'I':
                                 	pos_in_read += occurrence;
-                                	pos_in_gene += occurrence;
                                 	break;
                         	case 'H':
                                 	break;
                         	case 'P':
-                                	pos_in_gene += occurrence;
-                                	pos_in_read += occurrence;
                                 	break;
                         	case 'X':
                                 	pos_in_gene += occurrence;
